dd42db0c7d47345c424165581d6cedc5dfc236ee
Alvin Yuen <alvinyuen628@gmail.com>
2016-11-08 11:25:54 -0600

    add script and morgan

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = 3000;
 
 
-app.get('/', function(req, res){
-	res.send('welcome!!!!');
 
+//middleware
+app.use(morgan('dev'));
+
+// app.use('/', function(req, res, next){
+// 	console.log('GET /');
+// 	res.status(200);
+// 	next();
+// 	console.log('GET /'+res.status());
+// });
+
+
+// app.use('/news', function(req, res, next) {
+// 	console.log('GET /news');
+// 	next();
+// });
+
+
+//routing
+app.get('/', function(req, res, next){
+
+	res.send('Welcome!');
+});
+
+app.get('/news/election', function(req ,res, next){
+	res.send('some news!');
 });
 
 
+
+
+//listening PORT
 app.listen(PORT, function(){
 	console.log(`listening to ${PORT}!!`);
 });
